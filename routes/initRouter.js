2be99fbd8972c8ec90c6fc9f4617f6d31a1cbccb
Dhieu1304 <d.hieu.13.04@gmail.com>
2022-04-29 02:13:24 +0700

    add auth view (login, register, forgot-password

--- routes/initRouter.js
+++ routes/initRouter.js
@@ -1,5 +1,7 @@
 const homeRouter = require("./homeRouter");
+const authRouter = require("./authRouter");
 
 module.exports.initRouter = (app) => {
     app.use('/', homeRouter);
+    app.use('/auth', authRouter);
 }
