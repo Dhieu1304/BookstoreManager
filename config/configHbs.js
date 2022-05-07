499cefd69f05face0e711acd43c96c7accda8d96
Dhieu1304 <d.hieu.13.04@gmail.com>
2022-05-07 21:47:32 +0700

    update config (passport, hbs)

---
+++ config/configHbs.js
@@ -0,0 +1,33 @@
+const hbs = require("hbs");
+const expressHandlebarsSections = require("express-handlebars-sections");
+
+module.exports.configHbs = (dir) => {
+    hbs.registerPartials(dir);
+    hbs.registerHelper('section', expressHandlebarsSections());
+    hbs.registerHelper('ifCond', function (v1, operator, v2, options) {
+        switch (operator) {
+            case '==':
+                return (v1 == v2) ? options.fn(this) : options.inverse(this);
+            case '===':
+                return (v1 === v2) ? options.fn(this) : options.inverse(this);
+            case '!=':
+                return (v1 != v2) ? options.fn(this) : options.inverse(this);
+            case '!==':
+                return (v1 !== v2) ? options.fn(this) : options.inverse(this);
+            case '<':
+                return (v1 < v2) ? options.fn(this) : options.inverse(this);
+            case '<=':
+                return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+            case '>':
+                return (v1 > v2) ? options.fn(this) : options.inverse(this);
+            case '>=':
+                return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+            case '&&':
+                return (v1 && v2) ? options.fn(this) : options.inverse(this);
+            case '||':
+                return (v1 || v2) ? options.fn(this) : options.inverse(this);
+            default:
+                return options.inverse(this);
+        }
+    });
+}
