5e4112b765ecce6ffa8ee70ae697028eb69a8d1e
TranSangQST <72055149+TranSangQST@users.noreply.github.com>
2022-05-08 10:15:32 +0700

    sale UI/modal

--- public/js/sale.js
+++ public/js/sale.js
@@ -10,6 +10,30 @@ window.addEventListener('DOMContentLoaded', event => {
             paging: false,
         });
     }
+
+    const showBookTable = document.getElementById('showBookTable');
+    if (showBookTable) {
+        new simpleDatatables.DataTable(showBookTable, {
+        
+        });
+    }
+
+
 });
 
 
+function removeSaleReceiptDetailItem(index){
+    $("#saleReceiptDetailItem"+index).remove();
+}
+
+
+$("#isbnIp").on('keyup', function (e) {
+    if (e.key === 'Enter' || e.keyCode === 13) {
+        $("#isbnIp").val("");
+    }
+});
+
+
+$("#isbnIp").blur(function(){
+    $("#isbnIp").val("");
+});
