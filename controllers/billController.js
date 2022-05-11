9d927a09fd3af7e2d7bef2c8af85945772ecefd6
TranSangQST <72055149+TranSangQST@users.noreply.github.com>
2022-05-12 02:41:37 +0700

    State diagram

--- controllers/billController.js
+++ controllers/billController.js
@@ -8,54 +8,12 @@ exports.getBillPage = (req, res) => {
 
 exports.addBill = async (req, res) => {
 
-
-    // const data = req.body;
-
-    // const bookIds = data.bookIds;
-
-    // const quantitys = data.quantitys;
-
-    // const customerId = data.customerId;
-
-    // const prices = data.prices;
-
-    // let totalPrice = 0;
-
-    // if(prices){
-    //     if(Array.isArray(prices)){
-    //         totalPrice = prices.reduce(function(acc, val) { return acc + parseInt(val); }, 0);
-    //     }else{
-    //         totalPrice = prices;
-    //     }
-    // }
-
-    // const createAt = new Date();
-
-    // const bill = await billService.addBill(createAt, customerId, totalPrice);
-    // const billId = bill.id;
-
-    // const billDetailSize = bookIds.length;
-
-
-    // if(Array.isArray(prices)){
-    //     for await (const p of prices) {
-    //         const bookId = bookIds.shift();
-    //         const quantity = quantitys.shift();
-    //         const price = p;
-    //         const billDetail = await addBillDatail(bookId, billId, quantity, price)
-    //     }
-    // }else {
-    //     const bookId = bookIds;
-    //     const quantity = quantitys;
-    //     const price = prices;
-    //     const billDetail = await addBillDatail(bookId, billId, quantity, price)
-    // }
-
+    const data = req.body;
 
     // Test
     const createAt = new Date();
-    const customerId  = 1;
-    const money = 6;
+    const customerId  = data.id;
+    const money = data.money;
 
     const bill = billService.addBill(createAt, customerId, money);
 
--- routes/billRouter.js
+++ routes/billRouter.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const billController = require("../controllers/billController");
 
 router.get('/', billController.getBillPage);
-router.get('/add', billController.addBill);
+router.post('/add', billController.addBill);
 
 module.exports = router;
