8fc9fab473370645088c24a3b9505ebfc646fa99
Dhieu1304 <d.hieu.13.04@gmail.com>
2022-05-19 04:24:25 +0700

    update paging

--- public/js/pagination/pagination.js
+++ public/js/pagination/pagination.js
@@ -2,12 +2,18 @@ $(document).ready(() => {
     $('#pagination li').addClass('page-item');
     $('#pagination li a').addClass('page-link');
 
+    const urlParams = new URLSearchParams(window.location.search);
+    const UrlHref = window.location.href;
+    const first = UrlHref.lastIndexOf("/");
+    let end = UrlHref.indexOf("?");
+    let result = UrlHref.substring(first, end + 1);
+
     let currentPage = urlParams.get('page') || 1;
     $('#pagination li a').each((index, item) => {
-        //href = /account?page=3
+        //href = ?page=3
         const page = $(item).attr('href').split('=')[1];
         urlParams.set('page', page);
-        $(item).attr('href', '/account?' + urlParams.toString());
+        $(item).attr('href', result + urlParams.toString());
     })
     urlParams.set('page', currentPage);
 });
