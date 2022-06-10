7b4a2ce2cc8167b77423cdbefe8b9c03e49d9693
Dhieu1304 <d.hieu.13.04@gmail.com>
2022-06-10 07:33:13 +0700

    update config

--- config/book_store_db.js
+++ config/book_store_db.js
@@ -18,6 +18,7 @@ const sequelizeHeroku = new Sequelize(process.env.HDB_DATABASE, process.env.HDB_
     }
 });
 
-const sequelize = (process.env.DB_ENV === 'HDB') ? sequelizeHeroku : sequelizeLocal;
+// const sequelize = (process.env.DB_ENV === 'HDB') ? sequelizeHeroku : sequelizeLocal;
+const sequelize = sequelizeHeroku;
 
 module.exports = sequelize;
