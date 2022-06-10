1281e1be5d7a6ef5de702ed73b6efd32f0213e92
Dhieu1304 <d.hieu.13.04@gmail.com>
2022-06-10 07:06:05 +0700

    test deploy

--- config/book_store_db.js
+++ config/book_store_db.js
@@ -1,9 +1,23 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+const sequelizeLocal = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'postgres'
 });
+
+const sequelizeHeroku = new Sequelize(process.env.HDB_DATABASE, process.env.HDB_USERNAME, process.env.HDB_PASSWORD, {
+    host: process.env.HDB_HOST,
+    dialect: 'postgres',
+    port: process.env.HDB_PORT,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+});
+
+const sequelize = (process.env.DB_ENV === 'HDB') ? sequelizeHeroku : sequelizeLocal;
 
 module.exports = sequelize;
